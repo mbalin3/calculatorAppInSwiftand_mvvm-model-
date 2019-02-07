551d7a11c77a7deaf05d4d696a0854ed45409188
Mihlali Mazomba <Mish@Mishs-MacBook-Pro.local>
2019-02-07 08:59:53 +0200

    Initial Commit

---
+++ Calculator2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator2
+//
+//  Created by Mihlali Mazomba  on 2019/02/07.
+//  Copyright © 2019 Mihlali Mazomba . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
