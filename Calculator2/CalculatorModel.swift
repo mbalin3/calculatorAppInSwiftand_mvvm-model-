91b71a48f825b6e49e36fdbda50a73ceb175c751
Mihlali Mazomba <Mish@Mishs-MacBook-Pro.local>
2019-02-08 11:21:46 +0200

    this commit will update the ViewController

---
+++ Calculator2/CalculatorModel.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorModel.swift
+//  Calculator2
+//
+//  Created by Mihlali Mazomba  on 2019/02/07.
+//  Copyright © 2019 Mihlali Mazomba . All rights reserved.
+//
+
+import Foundation
+
+
+struct CalculatorModel{
+    var firstnumber :Int
+    var secondnumber  : Int
+    var numberoperator : String
+   
+}
